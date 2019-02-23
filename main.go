70573b76c2c9d0a1c0d8f1eb14340e5ff2d6b404
Ivan Kirillov <i_kirillov@wargaming.net>
2019-02-24 02:19:59 +0300

    add more structures

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ const ServerReqURL = APIServer + "/method/groups.getLongPollServer"
 const Wait = "25"
 const EventsReqURL = "%s?act=a_check&key=%s&ts=%s&wait=" + Wait
 
+var logger = log.New(os.Stdout, "gosocial ", log.Ltime)
+
 type Configuration struct {
 	API_KEY, GroupID string
 }
@@ -28,13 +30,25 @@ type LongPollParam struct {
 type APIResponse struct {
 	Response LongPollParam `json:"response"`
 	TS       string        `json:"ts"`
-	Updates  []string      `json:"updates"`
+	Updates  []UpdateEvent `json:"updates"`
 	Failed   int           `json:"failed"`
 }
 
 type VKPollClient struct {
 	APIKey, GroupID  string
 	SKey, Server, TS string
+}
+
+type UpdateEvent struct {
+	Type     string     `json:"type"`
+	GroupID  string     `json:"group"`
+	EventObj GroupEvent `json:"object"`
+}
+
+type GroupEvent struct {
+	UID       int    `json:"user_id"`
+	JoinType  string `json:"join_type,omitempty"`
+	LeaveType int    `json:"self,omitempty"`
 }
 
 func (vkcli *VKPollClient) GetLongPollServer() {
@@ -44,32 +58,49 @@ func (vkcli *VKPollClient) GetLongPollServer() {
 	res, err := http.Get(url)
 	defer res.Body.Close()
 	if err != nil {
-		log.Fatal(err)
+		logger.Print("[ERR} ", err)
 	}
 	body, _ := ioutil.ReadAll(res.Body)
 	jsonErr := json.Unmarshal([]byte(body), &answ)
 	if jsonErr != nil {
-		log.Fatal(jsonErr)
+		logger.Print("[ERR]", jsonErr)
 	}
 	vkcli.SKey = answ.Response.Key
 	vkcli.Server = answ.Response.Server
 	vkcli.TS = answ.Response.TS
 }
 
-func (vkcli VKPollClient) GetUpdates() APIResponse {
-	var updates APIResponse
+func (vkcli *VKPollClient) GetUpdates() ([]UpdateEvent, int) {
+	var api_resp APIResponse
 	url := fmt.Sprintf(EventsReqURL, vkcli.Server, vkcli.SKey, vkcli.TS)
 	res, err := http.Get(url)
 	defer res.Body.Close()
 	if err != nil {
-		log.Fatal(err)
+		logger.Print("[ERR]", err)
 	}
 	body, _ := ioutil.ReadAll(res.Body)
-	jsonErr := json.Unmarshal([]byte(body), &updates)
+	jsonErr := json.Unmarshal([]byte(body), &api_resp)
 	if jsonErr != nil {
-		log.Fatal(jsonErr)
+		logger.Print("[ERR]", jsonErr)
 	}
-	return (updates)
+
+	switch api_resp.Failed {
+	case 1:
+		// history is outdated or partly lost, try again with TS
+		// from current answer
+		vkcli.TS = api_resp.TS
+		return nil, 1
+	case 2:
+		// session key is expired
+		vkcli.GetLongPollServer()
+		return nil, 2
+	case 3:
+		// history is lost, request new key and ts
+		vkcli.GetLongPollServer()
+		return nil, 3
+	}
+	vkcli.TS = api_resp.TS
+	return api_resp.Updates, 0
 }
 
 func main() {
